3640449fbea922ebedd9dfdfcb94337cf5b775a9
Kernel02 <123792001+Kernel02@users.noreply.github.com>
2023-03-20 23:57:38 -0400

    final touches

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# challenge-4-code-quiz
-A timed coding quiz that can store high scores
+# Challenge 4
+
+## JavaScript Quiz
+
+A JavaScript quiz that can test one's knoweledge. I made this quiz to help myself and others be able to have fun studying the concepts of JavaScript. I made the entire page from scratch using HTML, CSS and JavaScript. Just recently I learned about third party APIs and the advantages to them. As I wrapped this up I can defnitely see how using those APIs would make this entire process much easier.
+
+![Screenshot of JavaScript Quiz page](./assets/images/Screenshot%202023-03-20%20at%2023.55.50.png)
+
+## Accessing the Website
+
+Click [here]() to access the website.
+
+## Usage
+
+Do your best at answering the questions on JavaScript. For every wrong question, the time will go down five seconds. Once the time is up or you have completed all of the questions, your score and initials can be saved. You are able to access the highscore page from the home page. As well, you can get back to the home page and clear the highscores from the highscore page.
+
+## License
+
+MIT License
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//Declaring all of the global variables
 var header = document.getElementById("header");
 var highscoreButton = document.getElementById("viewHighscores");
 var startButton = document.getElementById("startbutton");
@@ -12,6 +13,7 @@ var response = document.getElementById("response");
 var initials = document.getElementById("initials");
 var highscoreBoard = document.getElementById("highscoreBoard");
 var startAgainButton = document.getElementById("startAgain");
+var clearButton = document.getElementById("clear");
 var timeLeft = 60;
 var questionOn = 0;
 var answerChosen = "";
@@ -21,7 +23,7 @@ var savedHighscores = JSON.parse(localStorage.getItem("highscores"));
 if (savedHighscores) {
   highscores = savedHighscores;
 }
-console.log(highscores);
+//This quiz object holds all of the questions, answers and answer key
 var quiz = {
   questions: [
     "Which is a Boolean value?",
@@ -85,12 +87,12 @@ var quiz = {
   ],
   answerKey: ["", "B", "D", "A", "C", "C", "A", "D", "B", "D", "A"],
 };
-
+//This function starts the timer ticking down on the page
 function timerGo() {
-  timer.textContent = timeLeft;
+  timer.textContent = timeLeft + " seconds left";
   var timerRun = setInterval(function () {
     timeLeft--;
-    timer.textContent = timeLeft;
+    timer.textContent = timeLeft + " seconds left";
     if (timeLeft === 0 || timeLeft < 0) {
       clearInterval(timerRun);
     } else if (score > timeLeft) {
@@ -98,7 +100,7 @@ function timerGo() {
     }
   }, 1000);
 }
-
+//This function is used to hide the correct/wrong response after waiting one second
 function waitDisappear() {
   var waitTime = 1;
   var wait = setInterval(function () {
@@ -109,7 +111,7 @@ function waitDisappear() {
     }
   }, 1000);
 }
-
+//This function will hide the question, answers and timer. It will also set the timeLeft to 0 so that when the score pulls from the timeLeft variable, it will not be a negative integer(in the case that within 4 seconds of 0, the user gets a question wrong, taking the timeLeft to a negative number and ending the quiz)
 function stopQuiz() {
   answers.setAttribute("style", "display: none;");
   question.setAttribute("style", "display: none;");
@@ -121,7 +123,7 @@ function stopQuiz() {
   header.textContent = "Score: " + score;
   initials.setAttribute("style", "display: block;");
 }
-
+//Checks whether the quiz should end based on the question the user is on, then displays the next question, pulling from the quiz object
 function nextQuestion(questionNum) {
   if (questionOn === 10) {
     stopQuiz();
@@ -133,7 +135,7 @@ function nextQuestion(questionNum) {
     waitDisappear();
   } else {
     timeLeft = timeLeft - 5;
-    timer.textContent = timeLeft;
+    timer.textContent = timeLeft + " seconds left";
     if (timeLeft < 0) {
       stopQuiz();
       return;
@@ -147,43 +149,61 @@ function nextQuestion(questionNum) {
   C.textContent = quiz.answersC[questionNum];
   D.textContent = quiz.answersD[questionNum];
 }
-
+//Reveals our Home Page and Clear Highscores buttons. Uses a for loop to run through the array of scores and display them sorted on the screen
 function viewHighscores() {
   var scoreList = [];
   highscoreButton.setAttribute("style", "display: none");
   header.textContent = "Highscores";
   startButton.setAttribute("style", "display: none;");
-  sortedHighscores = highscores.sort((s1, s2) => (s1.userScore < s2.userScore) ? 1 : (s1.userScore > s2.userScore) ? -1 : 0);
+  sortedHighscores = highscores.sort((s1, s2) =>
+    s1.userScore < s2.userScore ? 1 : s1.userScore > s2.userScore ? -1 : 0
+  );
   for (var i = 0; i < highscores.length; i++) {
-    scoreList.push((i + 1) + ". " + sortedHighscores[i].userInitials + ": " + highscores[i].userScore);
+    scoreList.push(
+      i +
+        1 +
+        ". " +
+        sortedHighscores[i].userInitials +
+        ": " +
+        highscores[i].userScore
+    );
     var listEl = document.createElement("li");
     listEl.textContent = scoreList[i];
     highscoreBoard.appendChild(listEl);
     listEl.setAttribute("class", "highscores");
-    startAgainButton.setAttribute("style", "display: block");
   }
+  startAgainButton.setAttribute("style", "display: block");
+  clearButton.setAttribute("style", "display: block;");
 }
-
+//Listens for a click on the start button. Hides view highscores button and start button. Runs nextQuestion function
 startButton.addEventListener("click", function () {
   startButton.setAttribute("style", "display: none;");
+  highscoreButton.setAttribute("style", "display: none;");
   timerGo();
   answers.setAttribute("style", "display: block;");
   nextQuestion(questionOn);
   response.textContent = "";
 });
-
+//Grabs the answer chosen to check with the answer key within next question, to then display a correct or wrong statement. nextQuestion is run
 answers.addEventListener("click", function (event) {
   answerChosen = event.target.id;
   nextQuestion(questionOn);
 });
-
+//When clicked, run viewHighscores
 highscoreButton.addEventListener("click", viewHighscores);
-
+//When clicked, clear local storage and hide all currently displayed highscores
+clearButton.addEventListener("click", function (event) {
+  event.preventDefault;
+  localStorage.clear();
+  highscoreBoard.setAttribute("style", "display: none;");
+});
+//grabs the user score and initials and inserts it into local storage
 initials.children[2].addEventListener("click", function (event) {
   event.preventDefault();
   var userStats = {
-    userInitials: initials.children[1].value, userScore: score,
-  }
+    userInitials: initials.children[1].value,
+    userScore: score,
+  };
   highscores.push(userStats);
   localStorage.setItem("highscores", JSON.stringify(highscores));
   initials.setAttribute("style", "display: none;");
